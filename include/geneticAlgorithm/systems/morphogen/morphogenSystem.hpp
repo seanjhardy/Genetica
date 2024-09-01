b0e5eb55f1ba59530354cc168bbc01838aa8170f
Sean Hardy <seanjhardy@icloud.com>
2024-09-01 17:56:58 +0100

    Last point before major morphogen refactor

--- include/geneticAlgorithm/systems/morphogen/morphogenSystem.hpp
+++ include/geneticAlgorithm/systems/morphogen/morphogenSystem.hpp
@@ -5,23 +5,27 @@
 
 class MorphogenSystem {
 public:
-    void addMorphogen(int id, Morphogen morphogen) {
+    void addMorphogen(int id, Morphogen morphogen, std::unordered_map<int, float> morphogenInteractions) {
         morphogens.insert({id, morphogen});
+        concentrations.insert({id, 0.0});
+        for (const auto& interaction : morphogenInteractions) {
+            interactions.insert({{id, interaction.first}, interaction.second});
+        }
     }
 
-    [[nodiscard]] std::unordered_map<int, double> sampleAllMorphogens(const float2& point) const {
-        std::unordered_map<int, double> concentrations;
+    [[nodiscard]] std::unordered_map<int, float> update(const float2& point) {
         for (const auto& morphogen : morphogens) {
-            concentrations.insert({morphogen.first, morphogen.second.sample(point)});
+            concentrations[morphogen.first] = morphogen.second.sample(point);
         }
         // Compute morphogen interactions
         for (const auto& interaction : interactions) {
-            concentrations[interaction.first.first] += concentrations[interaction.first.second] * interaction.second;
+            concentrations[interaction.first.second] += concentrations[interaction.first.first] * interaction.second;
+            concentrations[interaction.first.first] *= (1.0 - abs(interaction.second));
         }
-        return concentrations;
     }
 
 private:
     std::unordered_map<int, Morphogen> morphogens;
+    std::unordered_map<int, float> concentrations;
     std::map<std::pair<int, int>, float> interactions;
 };
